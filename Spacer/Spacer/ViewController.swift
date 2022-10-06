07680f5a03f56ce58c7701f62c2ecc8ae6bb126f
한형서 <2017029243@hanyang.ac.kr>
2022-10-07 03:29:24 +0900

    Initial Commit

---
+++ Spacer/Spacer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spacer
+//
+//  Created by Hyung Seo Han on 2022/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+

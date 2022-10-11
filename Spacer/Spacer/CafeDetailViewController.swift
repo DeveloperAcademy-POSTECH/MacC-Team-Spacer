a24ab3c8b4881c0d82d1abd1afa5baab7e461882
bee712 <48711413+bee712@users.noreply.github.com>
2022-10-11 15:24:23 +0900

    feat: Create "CafeDetailViewController" file

---
+++ Spacer/Spacer/CafeDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CafeDetailViewController.swift
+//  Spacer
+//
+//  Created by Eunbi Han on 2022/10/11.
+//
+
+import UIKit
+
+class CafeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}

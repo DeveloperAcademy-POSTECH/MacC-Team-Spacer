d9fe0dcbf49e29840cc246750355bec51f297fdb
bee712 <48711413+bee712@users.noreply.github.com>
2022-10-11 13:46:06 +0900

    feat: Rename and Modify MainTabBarView
    - Rename MainTabBarView to "MainTabBarViewController"
    - 메인 탭 바에 안에 들어갈 viewcontroller 연결

---
+++ Spacer/Spacer/MainTabBarViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Spacer
+//
+//  Created by Hyung Seo Han on 2022/10/07.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let vc1 = UINavigationController(rootViewController: BirthdayCafeViewController())
+        let vc2 = UINavigationController(rootViewController: RequestViewController())
+        let vc3 = UINavigationController(rootViewController: ChatViewController())
+        let vc4 = UINavigationController(rootViewController: MyPageViewController())
+        
+        vc1.title = "생일카페"
+        vc2.title = "카페 구하기"
+        vc3.title = "채팅"
+        vc4.title = "내 정보"
+        
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+
+        
+    }
+
+
+}
+

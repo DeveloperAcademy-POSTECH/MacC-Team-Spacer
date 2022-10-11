8421ca7c5b2926fc30206d4eb32686c71e1bb3cf
bee712 <48711413+bee712@users.noreply.github.com>
2022-10-11 13:24:26 +0900

    feat: Create "FavoriteCafeView" file

---
+++ Spacer/Spacer/FavoriteCafeView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteCafeView.swift
+//  Spacer
+//
+//  Created by Eunbi Han on 2022/10/11.
+//
+
+import UIKit
+
+class FavoriteCafeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

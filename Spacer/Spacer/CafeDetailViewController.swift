cd4054e1d10c0cda3413fc182619aea1f531ed40
bee712 <48711413+bee712@users.noreply.github.com>
2022-10-12 15:51:06 +0900

    feat: Modify CafeDetailView data struct
    - CafeInfoForDetailView 데이터 구조 생성
    - 더미 데이터 생성
    - 기존 데이터 사용 변수 수정

--- Spacer/Spacer/CafeDetailViewController.swift
+++ Spacer/Spacer/CafeDetailViewController.swift
@@ -9,15 +9,18 @@ import UIKit
 
 class CafeDetailViewController: UIViewController, UIScrollViewDelegate {
     
-    // 상세 페이지에 필요한 카페 정보
-    let cafeName: String = "카페 로제"
-    let cafeImages: [String] = ["signature", "bag.fill", "creditcard.fill", "giftcard", "banknote", "dollarsign.circle.fill", "heart.fill"]
+    // 임시 카페 정보
+    private let cafeInfos: [CafeInfoForDetailView] = [
+        CafeInfoForDetailView(cafeID: 0, cafeName: "카페로제", imageDirectories: ["signature", "bag.fill", "creditcard.fill", "giftcard", "banknote", "dollarsign.circle.fill", "heart.fill"], address: "서울 홍대 어쩌고 저쩌고 106", cafePhoneNumber: "010-7189-8294", SNS: "@gumbee_h", cafeMinPeople: 20, cafeMaxPeople: 50, locationID: 0),
+        CafeInfoForDetailView(cafeID: 1, cafeName: "랑카페", imageDirectories: ["banknote", "bag.fill", "creditcard.fill", "giftcard", "signature", "dollarsign.circle.fill", "heart.fill"], address: "부산 어쩌고 저쩌고 11", cafePhoneNumber: "010-0000-8294", SNS: "@eunbi_Han", cafeMinPeople: 30, cafeMaxPeople: 40, locationID: 1)
+    ]
+    private let cafeIndex = 0
     
     // 카페 이미지를 볼 때 몇번째인지 표시하기 위한 PageControl
     lazy var pageControl: UIPageControl = {
         let pageControl = UIPageControl(frame: CGRect(x: 0, y: self.scrollView.bounds.maxY - 45, width: self.view.frame.maxX, height: 55))
         
-        pageControl.numberOfPages = cafeImages.count
+        pageControl.numberOfPages = cafeInfos[cafeIndex].imageDirectories.count
         pageControl.currentPage = 0
         pageControl.isUserInteractionEnabled = false
         
@@ -29,7 +32,7 @@ class CafeDetailViewController: UIViewController, UIScrollViewDelegate {
     lazy var scrollView: UIScrollView = {
         // ScrollView와 내부 Content Size 정의
         let scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 312))
-        scrollView.contentSize = CGSize(width: CGFloat(cafeImages.count) * self.view.bounds.width, height: 0)
+        scrollView.contentSize = CGSize(width: CGFloat(cafeInfos[cafeIndex].imageDirectories.count) * self.view.bounds.width, height: 0)
         
         // 스크롤 인디케이터 삭제
         scrollView.showsHorizontalScrollIndicator = false
@@ -53,7 +56,7 @@ class CafeDetailViewController: UIViewController, UIScrollViewDelegate {
         let scrollViewWidth = self.scrollView.bounds.width, scrollViewHeight = self.scrollView.bounds.height
         
         // 카페 이미지 보여주기
-        showCafeImages(width: scrollViewWidth, height: scrollViewHeight, cafeIamges: cafeImages, parentView: scrollView)
+        showCafeImages(width: scrollViewWidth, height: scrollViewHeight, cafeIamges: cafeInfos[cafeIndex].imageDirectories, parentView: scrollView)
         
         // scrollView, pageControl view에 추가
         self.view.addSubview(self.scrollView)
@@ -64,7 +67,7 @@ class CafeDetailViewController: UIViewController, UIScrollViewDelegate {
         for i in 0 ..< cafeIamges.count {
             // 카페 이미지 세팅
             let image = UIImageView()
-            image.image = UIImage(systemName: cafeImages[i])
+            image.image = UIImage(systemName: cafeInfos[cafeIndex].imageDirectories[i])
             image.contentMode = .scaleAspectFill
             image.frame = CGRect(x: CGFloat(i) * width, y: 0, width: width, height: height)
             
@@ -76,9 +79,23 @@ class CafeDetailViewController: UIViewController, UIScrollViewDelegate {
         // ScrollView에 보이는 페이지 이동이 끝나면 PageCtrol의 현재 위치 변경
         if fmod(scrollView.contentOffset.x, scrollView.frame.maxX) == 0 {
             pageControl.currentPage = Int(scrollView.contentOffset.x / self.scrollView.bounds.width)
-            
         }
-        
+
     }
     
 }
+
+// 임시 카페 정보 구조
+struct CafeInfoForDetailView: Codable {
+    let cafeID: Int
+    let cafeName: String
+    let imageDirectories: [String]
+    let address: String
+    let cafePhoneNumber: String
+    var SNS: String = ""
+    let cafeMinPeople: Int
+    let cafeMaxPeople: Int
+    var costs: Int = 0
+    let locationID: Int
+}
+

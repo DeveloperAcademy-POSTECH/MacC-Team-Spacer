00081807b55171e032d3635d0e9b6d975aadc966
dasol <hurdasol@naver.com>
2022-10-18 16:24:18 +0900

    feat: Add RecentCafeTableViewCell
    - Section2의 테이블 셀 RecentCafeTableViewCell 구현

---
+++ Spacer/Spacer/RecentCafeTableViewCell.swift
@@ -0,0 +1,72 @@
+//
+//  RecentCafeTableViewCell.swift
+//  Spacer
+//
+//  Created by hurdasol on 2022/10/13.
+//
+
+import UIKit
+// CollectionView를 담을 1개 짜리 테이블 셀
+class RecentCafeTableViewCell: UITableViewCell {
+
+    public var tempCafeArray: [CafeInfo] = [CafeInfo]()
+    
+    static let identifier = "RecentCafeTableViewCell"
+    
+    var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 252, height: 120)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.identifier)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    public func configure(with model: [CafeInfo]) {
+        self.tempCafeArray = model
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
+    }
+    
+}
+
+extension RecentCafeTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tempCafeArray.count
+        
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.identifier, for: indexPath) as? CollectionViewCell else { return UICollectionViewCell() }
+        // MARK: - 1. 셀에 값 넘기기
+        cell.configure(with: tempCafeArray[indexPath.row])
+        
+        return cell
+    }
+    
+
+
+}

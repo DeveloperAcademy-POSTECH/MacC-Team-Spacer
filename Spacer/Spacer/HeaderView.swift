06efbb5fce495d8a61a09beaf8495e95aa5d1fca
dasol <hurdasol@naver.com>
2022-10-18 16:02:33 +0900

    feat: ADD HeaderView
    - BirthdayCafeView에 들어갈 headerView 추가

---
+++ Spacer/Spacer/HeaderView.swift
@@ -0,0 +1,65 @@
+//
+//  HeaderView.swift
+//  Spacer
+//
+//  Created by hurdasol on 2022/10/11.
+//
+
+import UIKit
+
+class MyHeaderView: UIView {
+    
+    static let identifier = "MyHeaderView"
+    
+    private let headerImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "RANG")
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(headerImageView)
+    }
+    
+    private func applyConstraints() {
+        
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        headerImageView.frame = bounds
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+class logoView: UIView {
+    static let identifier = "logoView"
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.image = UIImage(named: "RANG")
+        imageView.frame = .init(x: 0, y: 0, width: 50, height: 10)
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(logoImageView)
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        logoImageView.frame = bounds
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

c8036a88f53f70c5275420389c1dfeecc7565dcc
dasol <hurdasol@naver.com>
2022-10-18 16:26:50 +0900

    feat: Add PopularCafeTableViewCell & CollectionViewCell
    - Section1의 테이블 셀 PopularCafeTableViewCell구현
    - 1개의 테이블 셀 안에서 horizontal한 컬렉션뷰,셀 CollectionViewCell 구현

---
+++ Spacer/Spacer/CollectionViewCell.swift
@@ -0,0 +1,116 @@
+//
+//  CollectionViewCell.swift
+//  Spacer
+//
+//  Created by hurdasol on 2022/10/11.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    static let identifier = "TempCollectionViewCell"
+    
+    
+    var cafeImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 12
+        imageView.contentMode = .scaleAspectFill
+        // 부분만 둥글게 하기
+        imageView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    var cafeName: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    let cafeStarRating: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor(red: 255/255, green: 92/255, blue: 92/255, alpha: 1)
+        label.font = UIFont(name: "Pretendard", size: 16)
+        
+        return label
+    }()
+    var cafeReview: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont(name: "Pretendard", size: 14)
+        label.text = "리뷰의 내용이 들어가야합니다 어떤게 어쩔 티비 저쩔 티비야 뇌절 티비"
+        label.textAlignment = .left
+        label.lineBreakMode = .byWordWrapping
+        label.allowsDefaultTighteningForTruncation = true
+        label.numberOfLines = 4
+        return label
+        
+    }()
+    
+    // 그라디언트
+    let gradientLayer = CAGradientLayer()
+    override func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+        gradientLayer.frame = cafeImageView.bounds
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(cafeImageView)
+        addSubview(cafeName)
+        addSubview(cafeStarRating)
+        addSubview(cafeReview)
+        let cafeImageViewConstraints = [
+            cafeImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            cafeImageView.topAnchor.constraint(equalTo: topAnchor),
+            cafeImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            cafeImageView.widthAnchor.constraint(equalToConstant: self.frame.width/2 ),
+        ]
+        let cafeNameConstraints = [
+            cafeName.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            cafeName.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+            cafeName.heightAnchor.constraint(equalToConstant: 20)
+        ]
+        let cafeStarRatingConstraints = [
+            cafeStarRating.leadingAnchor.constraint(equalTo: cafeImageView.trailingAnchor, constant: 16),
+            cafeStarRating.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            
+        ]
+        let cafeReviewConstraints = [
+            cafeReview.leadingAnchor.constraint(equalTo: cafeStarRating.leadingAnchor),
+            cafeReview.topAnchor.constraint(equalTo: cafeStarRating.bottomAnchor, constant: 4),
+            cafeReview.trailingAnchor.constraint(equalTo: trailingAnchor,constant: -10)
+        ]
+        
+        NSLayoutConstraint.activate(cafeImageViewConstraints)
+        NSLayoutConstraint.activate(cafeNameConstraints)
+        NSLayoutConstraint.activate(cafeStarRatingConstraints)
+        NSLayoutConstraint.activate(cafeReviewConstraints)
+        contentView.layer.cornerRadius = 12
+        contentView.layer.borderWidth = 1
+        contentView.layer.borderColor = UIColor.green.cgColor
+        gradientLayer.frame = contentView.bounds
+        let colorSet = [UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.0),
+                        UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1),
+                        ]
+        let location = [0.5,1.0]
+        cafeImageView.addGradient(with: gradientLayer, colorSet: colorSet, locations: location)
+    }
+    
+
+    // MARK: - 1. cafeInfo를 받아와서 셀에 값을 넣어줌
+    public func configure(with model: CafeInfo) {
+        self.cafeName.text = model.cafe_name
+        self.cafeImageView.image = UIImage(named: model.image_directories)
+        self.cafeStarRating.text = String("☆")+String(model.cafe_star_rating)
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Spacer/Spacer/PopularCafeTableViewCell.swift
@@ -0,0 +1,159 @@
+//
+//  PopularCafeTableViewCell.swift
+//  Spacer
+//
+//  Created by hurdasol on 2022/10/11.
+//
+
+import UIKit
+
+class PopularCafeTableViewCell: UITableViewCell {
+    
+    static let identifier = "PopularCafeTableViewCell"
+    // 카페 이미지
+    private let CafeImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        
+        imageView.image = UIImage(named: "RANG")
+        imageView.layer.cornerRadius = 12
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    // 그라디언트
+    let gradientLayer = CAGradientLayer()
+    // 카페 이름
+    private let cafeName: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "랑카페"
+        return label
+    }()
+    // 카페 별점
+    private let cafeStarRating: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor(red: 255/255, green: 92/255, blue: 92/255, alpha: 1)
+        label.text = "⭐️4.5(32)"
+        return label
+    }()
+    
+    // 카페 위치
+    private let cafeLocation: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "📍서울 홍대"
+        return label
+    }()
+    // 카페 인원수
+    private let cafePeople: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "👤20~40"
+        return label
+    }()
+    // 즐겨찾기-하트
+    private let cafeHeart: UIButton = {
+        let button = UIButton(type: .custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "heart"), for: .normal)
+        
+        return button
+        
+    }()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(CafeImageView)
+        contentView.addSubview(cafeName)
+        contentView.addSubview(cafeStarRating)
+        contentView.addSubview(cafeLocation)
+        contentView.addSubview(cafePeople)
+        contentView.addSubview(cafeHeart)
+        applyConstraints()
+        
+    }
+    
+    private func applyConstraints() {
+        let cafeNameConstraints = [
+            cafeName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            cafeName.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 134),
+            cafeName.heightAnchor.constraint(equalToConstant: 20),
+//            cafeName.widthAnchor.constraint(equalToConstant: 45),
+        ]
+        let cafeStarRatingConstraints = [
+            cafeStarRating.leadingAnchor.constraint(equalTo: cafeName.trailingAnchor, constant: 5),
+            cafeStarRating.heightAnchor.constraint(equalToConstant: 20),
+            cafeStarRating.topAnchor.constraint(equalTo: cafeName.topAnchor),
+        ]
+       let cafeLocationConstraints = [
+            cafeLocation.leadingAnchor.constraint(equalTo: cafeName.leadingAnchor),
+            cafeLocation.topAnchor.constraint(equalTo: cafeName.bottomAnchor, constant: 5),
+            cafeLocation.heightAnchor.constraint(equalToConstant:20)
+       ]
+        let cafePeopleConstraints = [
+            cafePeople.leadingAnchor.constraint(equalTo: cafeLocation.trailingAnchor, constant: 5),
+            cafePeople.topAnchor.constraint(equalTo: cafeLocation.topAnchor),
+            cafePeople.heightAnchor.constraint(equalToConstant:20),
+        ]
+        let cafeHeartConstarints = [
+            cafeHeart.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            cafeHeart.topAnchor.constraint(equalTo: cafeLocation.topAnchor),
+            cafeHeart.heightAnchor.constraint(equalToConstant: 20),
+        ]
+        NSLayoutConstraint.activate(cafeNameConstraints)
+        NSLayoutConstraint.activate(cafeStarRatingConstraints)
+        NSLayoutConstraint.activate(cafeLocationConstraints)
+        NSLayoutConstraint.activate(cafePeopleConstraints)
+        NSLayoutConstraint.activate(cafeHeartConstarints)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 6, left: 20, bottom: 7, right: 20))
+        NSLayoutConstraint.activate([
+            CafeImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            CafeImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            CafeImageView.widthAnchor.constraint(equalToConstant: contentView.frame.width),
+            CafeImageView.heightAnchor.constraint(equalToConstant: contentView.frame.height),
+        ])
+        let colorSet = [UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.1),
+                        UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.8),
+                        UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)]
+        let location = [0.5,0.7,1.0]
+        CafeImageView.addGradient(with: gradientLayer, colorSet: colorSet, locations: location)
+        
+    }
+    // MARK: - 1. cafeInfo를 받아와서 셀에 값을 넣어줌
+    public func configure(with model: CafeInfo) {
+        self.cafeName.text = model.cafe_name
+        self.CafeImageView.image = UIImage(named: model.image_directories)
+        self.cafeStarRating.text = "⭐️" + String(model.cafe_star_rating)
+        self.cafeLocation.text = "📍" + String(model.location_id) + "지역"
+        self.cafePeople.text = "👤 \(model.cafe_min_people) ~ \(model.cafe_max_people)"
+        
+    }
+    
+    // MARK: - layer에다 그라디언트 추가하기
+    override func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+
+        gradientLayer.frame = contentView.bounds
+        
+        
+        
+    }
+    
+    
+    
+    
+}
+
+

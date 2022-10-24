7587c1894a569d1a36bfa4b42d76c03716a1f4df
bee712 <48711413+bee712@users.noreply.github.com>
2022-10-24 12:33:28 +0900

    feat: Implement InfomationImageAndText UI
    - 생성자를 두 개 만들어 받아오는 정보에 따라 다른 UI가 생성되도록 하였습니다
    - view 사이의 오토레이아웃을 위한 제약사항을 설정하였습니다

--- Spacer/Spacer/Views/CafeDetailView/InfomationImageAndText.swift
+++ Spacer/Spacer/Views/CafeDetailView/InfomationImageAndText.swift
@@ -8,6 +8,90 @@
 import UIKit
 
 class InfomationImageAndText: UIView {
-
     
+    lazy var icon: UIImageView = {
+        let icon = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        icon.tintColor = .mainPurple4
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        return icon
+    }()
+    
+    lazy var category: UILabel = {
+        let category = UILabel()
+        category.font = .systemFont(for: .body3)
+        category.textColor = .grayscale1
+        category.translatesAutoresizingMaskIntoConstraints = false
+        return category
+    }()
+    
+    lazy var discription: UILabel = {
+        let discription = UILabel()
+        discription.font = .systemFont(for: .body3)
+        discription.textColor = .grayscale1
+        discription.translatesAutoresizingMaskIntoConstraints = false
+        return discription
+    }()
+    
+    init(image: String, discription: String) {
+        super.init(frame: CGRect())
+        
+        icon.image = UIImage(systemName: image)
+        self.discription.text = discription
+        
+        self.addSubview(icon)
+        self.addSubview(self.discription)
+        
+        applyIconConstraints()
+        applyDiscriptionConstraints(isCategoryText: false)
+    }
+     
+    init(image: String, category: String, discription: String) {
+        super.init(frame: CGRect())
+        
+        icon.image = UIImage(systemName: image)
+        self.category.text = category
+        self.discription.text = discription
+        
+        self.addSubview(icon)
+        self.addSubview(self.category)
+        self.addSubview(self.discription)
+        
+        applyIconConstraints()
+        applyCategoryConstraints()
+        applyDiscriptionConstraints(isCategoryText: true)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func applyIconConstraints() {
+        let iconConstraints = [
+            icon.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            icon.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(iconConstraints)
+    }
+    
+    private func applyCategoryConstraints() {
+        let categoryConstraints = [
+            category.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 8),
+            category.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            category.widthAnchor.constraint(equalToConstant: 52)
+        ]
+
+        NSLayoutConstraint.activate(categoryConstraints)
+    }
+    
+    private func applyDiscriptionConstraints(isCategoryText: Bool) {
+        let discriptionConstraints = [
+            discription.leadingAnchor.constraint(equalTo: isCategoryText ? category.trailingAnchor : icon.trailingAnchor, constant: isCategoryText ? 18 : 8),
+            discription.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(discriptionConstraints)
+    }
+
+
 }

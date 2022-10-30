7f4d2e96940151df399d22e34c2732aba26912ea
hurdasol98 <73656470+hurdasol98@users.noreply.github.com>
2022-10-30 18:09:59 +0900

    feat: Renaming viewController in MainTabBarViewController
    - 뷰 컨트롤러에 대한 이름을 자세하게 바꿈

--- Spacer/Spacer/Cores/MainTabBarViewController.swift
+++ Spacer/Spacer/Cores/MainTabBarViewController.swift
@@ -14,9 +14,9 @@ class MainTabBarViewController: UITabBarController {
         
         view.backgroundColor = .systemBackground
         
-        let vc1 = UINavigationController(rootViewController: BirthdayCafeViewController())
-        let vc2 = UINavigationController(rootViewController: RequestViewController())
-        let vc3 = UINavigationController(rootViewController: MyPageViewController())
+        let birthdayCafeViewController = UINavigationController(rootViewController: BirthdayCafeViewController())
+        let requestViewController = UINavigationController(rootViewController: RequestViewController())
+        let MyPageViewController = UINavigationController(rootViewController: MyPageViewController())
         
         // 선택되었을 경우 색상
         self.tabBar.tintColor = .mainPurple3
@@ -24,17 +24,17 @@ class MainTabBarViewController: UITabBarController {
         // tabBar가 선택되지 않았을 때의 색상
         self.tabBar.unselectedItemTintColor = .mainPurple3
         
-        let vc1TabBarItem = UITabBarItem(title: "생일카페", image:  UIImage(named: "BirthdayCafe_Outline")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "BirthdayCafe_Fill")?.withRenderingMode(.alwaysOriginal))
-        vc1.tabBarItem = vc1TabBarItem
+        let birthdayCafeViewControllerTabBarItem = UITabBarItem(title: "생일카페", image:  UIImage(named: "BirthdayCafe_Outline")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "BirthdayCafe_Fill")?.withRenderingMode(.alwaysOriginal))
+        birthdayCafeViewController.tabBarItem = birthdayCafeViewControllerTabBarItem
         
-        let vc2TabBarItem = UITabBarItem(title: "카페 구하기", image: UIImage(named: "Request_Outline")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "Request_Fill")?.withRenderingMode(.alwaysOriginal))
-        vc2.tabBarItem = vc2TabBarItem
+        let requestViewControllerTabBarItem = UITabBarItem(title: "카페 구하기", image: UIImage(named: "Request_Outline")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "Request_Fill")?.withRenderingMode(.alwaysOriginal))
+        requestViewController.tabBarItem = requestViewControllerTabBarItem
         
-        let vc3TabBarItem = UITabBarItem(title: "내 정보", image: UIImage(named: "MyPage_Outline")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "MyPage_Fill")?.withRenderingMode(.alwaysOriginal))
-        vc3.tabBarItem = vc3TabBarItem
+        let MyPageViewControllerTabBarItem = UITabBarItem(title: "내 정보", image: UIImage(named: "MyPage_Outline")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "MyPage_Fill")?.withRenderingMode(.alwaysOriginal))
+        MyPageViewController.tabBarItem = MyPageViewControllerTabBarItem
         
         
-        setViewControllers([vc1, vc2, vc3], animated: true)
+        setViewControllers([birthdayCafeViewController, requestViewController, MyPageViewController], animated: true)
     }
     
 }

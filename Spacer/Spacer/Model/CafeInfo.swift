a085ba753283d7478b6495fa0b85787fc3689266
bee712 <48711413+bee712@users.noreply.github.com>
2022-11-04 15:26:34 +0900

    feat: Change CafeInfo
    - 받아올 데이터에 따라 변수와 구조체 추가 및 삭제

--- Spacer/Spacer/Model/CafeInfo.swift
+++ Spacer/Spacer/Model/CafeInfo.swift
@@ -8,16 +8,43 @@
 import Foundation
 
 struct CafeInfo: Codable {
-    let cafe_id: Int
-    let cafe_name: String
-    let image_directories: [String]
+    let ID: String
+    let name: String
+    let imageInfos: [ImageInfo]
+    let locationID: Int
     let address: String
-    let cafe_phone_number: String
-    let sns: String?
-    let cafe_min_people: Int
-    let cafe_max_people: Int
-    let costs: Int?
-    let location_id: Int
-    let cafe_star_rating: Double
-
+    let shortAddress: String
+    let numberOfFavorites: Int
+    let numberOfTables: Int
+    let phoneNumber: String
+    let SNS: SNSList
+    let weekdayTime: String
+    let weekendTime: String
+    let dayOff: String?
+    let eventElement: [Bool]
+    let cost: Int
+    let additionalInfo: String?
+    let reviews: [CafeReview]?
 }
+
+struct SNSList: Codable {
+    let insta: String?
+    let twitter: String?
+}
+
+struct ImageInfo: Codable {
+    let images: [String]
+    let category: String
+    let productSize: String
+}
+
+struct CafeReview: Codable {
+    let reviewID: Int
+    let userID: String
+    let userNickname: String
+    let date: Date
+    let selectedLabel: [Int]?
+    let images: [String]?
+    let text: String
+}
+

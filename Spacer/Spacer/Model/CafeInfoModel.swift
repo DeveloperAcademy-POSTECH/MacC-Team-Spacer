ca06eda0dfbaa9d1493139443432cb466cacb9aa
bee712 <48711413+bee712@users.noreply.github.com>
2022-11-04 15:28:19 +0900

    feat: Modify CafeInfoModel
    - 더 이상 사용하지 않는 기존 구조 주석 처리

--- Spacer/Spacer/Model/CafeInfoModel.swift
+++ Spacer/Spacer/Model/CafeInfoModel.swift
@@ -7,36 +7,36 @@
 
 import Foundation
 
-struct CafeInfoModel: Codable {
-    let cafeID: Int
-    let cafeName: String
-    let imageDirectories: [String]
-    let cafeAddress: String
-    let cafePhoneNumber: String?
-    let SNS: SNSList
-    let weekdayTime: String
-    let saturdayTime: String?
-    let sundayTime: String?
-    let holidayTime: String?
-    let cafeDayOff: String?
-    let cafeMinPeople: Int?
-    let cafeMaxPeople: Int?
-    let cafeCosts: CostsList
-    let locationID: Int?
-    let cafeStarRating: Double
-    let cafeEventElement: [Int]?
-    let cafeAdditionalInfo: String?
-}
-
-struct SNSList: Codable {
-    let insta: String?
-    let twitter: String?
-    let facebook: String?
-    let homepage: String?
-}
-
-struct CostsList: Codable {
-    let rentalFee: Int?
-    let deposit: Int?
-    let reservartion: Int?
-}
+//struct CafeInfoModel: Codable {
+//    let cafeID: Int
+//    let cafeName: String
+//    let imageDirectories: [String]
+//    let cafeAddress: String
+//    let cafePhoneNumber: String?
+//    let SNS: SNSList
+//    let weekdayTime: String
+//    let saturdayTime: String?
+//    let sundayTime: String?
+//    let holidayTime: String?
+//    let cafeDayOff: String?
+//    let cafeMinPeople: Int?
+//    let cafeMaxPeople: Int?
+//    let cafeCosts: CostsList
+//    let locationID: Int?
+//    let cafeStarRating: Double
+//    let cafeEventElement: [Int]?
+//    let cafeAdditionalInfo: String?
+//}
+//
+//struct SNSList: Codable {
+//    let insta: String?
+//    let twitter: String?
+//    let facebook: String?
+//    let homepage: String?
+//}
+//
+//struct CostsList: Codable {
+//    let rentalFee: Int?
+//    let deposit: Int?
+//    let reservartion: Int?
+//}

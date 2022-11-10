e6f3289dab8cc0a25ce45602c2e4964a0c7d860e
hurdasol98 <hurdasol@naver.com>
2022-11-10 18:30:53 +0900

    feat: Delete UserDefaultsValue when app terminated
    - VisualTagView에서 선택한 상태로 SearchListView에서 앱을 종료하더라도 userDefaults에 저장된 값이라서 유지되는 문제 앱을 종료하였을 경우 userDefaults에 저장된 필터Tag삭제하여 해결함

--- Spacer/Spacer/Cores/AppDelegate.swift
+++ Spacer/Spacer/Cores/AppDelegate.swift
@@ -31,6 +31,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    //앱을 종료하였을 경우 userDefaults에 저장된 필터Tag삭제
+    func applicationWillTerminate(_ application: UIApplication) {
+        UserDefaults.standard.removeObject(forKey: "categories")
+        UserDefaults.standard.removeObject(forKey: "map")
+        UserDefaults.standard.removeObject(forKey: "firstDate")
+        UserDefaults.standard.removeObject(forKey: "lastDate")
+    }
 
 }
 

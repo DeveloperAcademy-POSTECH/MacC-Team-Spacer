9343b6b73dda27f06453603d391be97f09fb1d88
bee712 <48711413+bee712@users.noreply.github.com>
2022-11-21 03:05:04 +0900

    feat: Add function requestGetData
    - 데이터 요청을 할 requestGetData 함수 생성

--- Spacer/Spacer/Managers/APICaller.swift
+++ Spacer/Spacer/Managers/APICaller.swift
@@ -9,4 +9,39 @@ import Foundation
 
 class APICaller {
     
+    static let shared = APICaller()
+    private let baseURL = "http://158.247.222.189:12232"
+    
+    // API로 데이터 요청
+    func requestGetData(url: String, dataType: DataType, completionHandler: @escaping (Bool, Any) -> Void) {
+        guard let url = URL(string: baseURL + url) else {
+            print("Error: cannot create URL")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                print("Error: error calling GET")
+                return
+            }
+            guard let data = data else {
+                print("Error: Did not receive data")
+                return
+            }
+            guard let response = response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
+                print("Error: HTTP request failed")
+                return
+            }
+            guard let output = try? JSONDecoder().decode(Cafeinfo.self, from: data) else {
+                print("Error: JSON Data Parsing failed")
+                return
+            }
+            
+            completionHandler(true, output)
+        }.resume()
+    }
+    
 }

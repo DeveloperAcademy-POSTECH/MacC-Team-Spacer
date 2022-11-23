630e717e4c815ff0d0de4e08c56c4e2fd0924970
hurdasol98 <73656470+hurdasol98@users.noreply.github.com>
2022-11-23 19:52:36 +0900

    feat: Enable PopGesture
    - NavigationBar를 hidden시 사라지는 swipe하여 뒤로가기 기능을 위해 navigationStack에 view가 1개 보다 많을 경우 interactivePopGestureRecognizer를 인식하여 동작함

---
+++ Spacer/Spacer/Extensions/UINavigationControllerExtension.swift
@@ -0,0 +1,21 @@
+//
+//  UINavigationControllerExtension.swift
+//  Spacer
+//
+//  Created by 허다솔 on 2022/11/23.
+//
+
+import UIKit
+
+// NavigationBar를 hidden처리하면서 기본으로 제공하는 swipe하여 뒤로가기 기능 사라짐
+// -> navigationStack에 view가 1개 보다 많을 경우 interactivePopGestureRecognizer를 인식하여 동작
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}

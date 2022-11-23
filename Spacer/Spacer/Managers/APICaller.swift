4db413f34402521db95ad2385089d14a941236cb
bee712 <48711413+bee712@users.noreply.github.com>
2022-11-23 17:12:24 +0900

    feat: Modify requestGetData and Add APIError
    - requestGetData 함수를 async, await를 사용한 형태로 변경
    - error를 전송하기 위해  APIError 타입 추가

--- Spacer/Spacer/Managers/APICaller.swift
+++ Spacer/Spacer/Managers/APICaller.swift
@@ -7,41 +7,26 @@
 
 import Foundation
 
+enum APIError: Error {
+    case HTTPError
+    case DataParsingError
+}
+
 class APICaller {
     // 공통 url 주소
     private static let baseURL = "http://158.247.222.189:12232"
     
-    // API로 데이터 요청
-    static func requestGetData<T: Codable>(url: String, dataType: T.Type, completionHandler: @escaping (Bool, Any) -> Void) {
-        guard let url = URL(string: baseURL + url) else {
-            print("Error: cannot create URL")
-            return
-        }
-        
-        var request = URLRequest(url: url)
+    // Get Data
+    static func requestGetData<T: Codable>(url: String, dataType: T.Type) async throws -> Any  {
+        let url = URL(string: baseURL + url)
+        var request = URLRequest(url: url!)
         request.httpMethod = "GET"
         
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            guard error == nil else {
-                print("Error: error calling GET")
-                return
-            }
-            guard let data = data else {
-                print("Error: Did not receive data")
-                return
-            }
-            guard let response = response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
-                print("Error: HTTP request failed")
-                return
-            }
-            guard let output = try? JSONDecoder().decode(dataType, from: data) else {
-                print("Error: JSON Data Parsing failed")
-                return
-            }
-            DispatchQueue.main.async {
-                completionHandler(true, output)
-            }
-        }.resume()
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw APIError.HTTPError }
+        guard let output = try? JSONDecoder().decode(dataType, from: data) else { throw APIError.DataParsingError }
+        
+        return output
     }
     
 }

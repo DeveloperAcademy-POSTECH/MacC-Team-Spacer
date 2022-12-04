37b9d7230075d252a0d32d43741625f5b7c8364e
bee712 <48711413+bee712@users.noreply.github.com>
2022-12-04 18:45:37 +0900

    style: Rename interactivePopIsEnable to isInteractivePopEnable
    - Swift 스타일에 맞게 함수명 변경

--- Spacer/Spacer/Extensions/UINavigationControllerExtension.swift
+++ Spacer/Spacer/Extensions/UINavigationControllerExtension.swift
@@ -10,7 +10,7 @@ import UIKit
 // NavigationBar를 hidden처리하면서 기본으로 제공하는 swipe하여 뒤로가기 기능 사라짐
 // -> navigationStack에 view가 1개 보다 많을 경우 interactivePopGestureRecognizer를 인식하여 동작
 extension UINavigationController: UIGestureRecognizerDelegate {
-    public func interactivePopIsEnable(_ bool: Bool) {
+    public func isInteractivePopEnable(_ bool: Bool) {
         if bool == true {
             interactivePopGestureRecognizer?.delegate = self
         }
